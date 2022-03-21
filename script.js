189dc0e581efb1be7e7b71974d062cdcaaea3849
jdeymisson <jdeymisson@gmail.com>
2022-03-21 10:10:59 -0300

    animation section

---
+++ script.js
@@ -0,0 +1,5 @@
+const inputRange = document.querySelector('#simulate');
+
+inputRange.addEventListener('input', () => {
+    let display = document.querySelector('.simulate').innerText = `R$ ${inputRange.value}`;
+});
