3ec83352f70accd6269da75c38873f755500dca5
jdeymisson <jdeymisson@gmail.com>
2022-03-21 15:08:32 -0300

    hSetting the value number to floating point.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const inputRange = document.querySelector('#simulate');
 
 inputRange.addEventListener('input', () => {
-    let display = document.querySelector('.simulate').innerText = `R$ ${inputRange.value}`;
+    let display = document.querySelector('.simulate').innerText = `R$ ${inputRange.value * 1.0}`.replace('.', ',');
 });
 
