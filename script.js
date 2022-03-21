076d007f1ea69cfc260bcaa27d47ec43b67e6d9a
jdeymisson <jdeymisson@gmail.com>
2022-03-21 12:02:28 -0300

    adjustment in the demonstration section

--- script.js
+++ script.js
@@ -3,3 +3,4 @@ const inputRange = document.querySelector('#simulate');
 inputRange.addEventListener('input', () => {
     let display = document.querySelector('.simulate').innerText = `R$ ${inputRange.value}`;
 });
+
