a55ce8bb0ba70ee8d02ebef5412845395c2dc723
Johnny <jdeymisson@gmail.com>
2022-03-23 20:32:02 -0300

    Trigg bank project finished and responsive.

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-const inputRange = document.querySelector('#simulate');
+const inputRange = document.querySelector("#simulate");
 
-inputRange.addEventListener('input', () => {
-    let display = document.querySelector('.simulate').innerText = `R$ ${inputRange.value * 1.0}`.replace('.', ',');
+inputRange.addEventListener("input", () => {
+  let display = (document.querySelector(".simulate").innerText = `R$ ${
+    inputRange.value * 1.0
+  }`.replace(".", ","));
 });
 
+const btnMobile = document.querySelector(".btn-mobile");
+
+function menuToggle() {
+  const nav = document.querySelector(".nav");
+  nav.classList.toggle("active");
+}
+
+btnMobile.addEventListener("click", menuToggle);
